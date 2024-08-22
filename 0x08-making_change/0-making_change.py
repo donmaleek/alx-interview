85049d51a3336e83f21809ba31ce8ce45723f5df
donmaleek <bamboloshari@gmail.com>
2024-08-22 12:33:32 -0700

    update 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """0x19. Making Change"""
 
+
 def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
     if total <= 0:
@@ -9,9 +10,9 @@ def makeChange(coins, total):
     val = [float('inf')] * (total + 1)
     val[0] = 0
 
-    for i in range(1, total + 1):
+    for i in range(1, len(val)):
         for j in range(len(coins)):
             if coins[j] <= i:
                 val[i] = min(val[i], val[i - coins[j]] + 1)
 
-    return val[total] if val[total] != float('inf') else -1
+    return val[i] if val[i] != float('inf') else -1
