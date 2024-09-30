69bf5fd2f7629526e14288bf6d88dc47f33b222a
donmaleek <bamboloshari@gmail.com>
2024-09-30 22:15:14 +0300

    update of of main_py for pep 8

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
 """Module defining isWinner function."""
 
-def isWinner(x, nums):
-    """Function to get who has won in prime game"""
+
+def is_winner(x, nums):
+    """Function to determine who has won in the prime game."""
     if x == 0 or not nums:
         return None
 
-    mariaWinsCount = 0
-    benWinsCount = 0
+    maria_wins_count = 0
+    ben_wins_count = 0
 
     # Iterate over each game round
     for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+        rounds_set = list(range(1, num + 1))
+        primes_set = primes_in_range(1, num)
 
         # If no prime numbers exist, Ben wins by default
-        if not primesSet:
-            benWinsCount += 1
+        if not primes_set:
+            ben_wins_count += 1
             continue
 
-        isMariaTurns = True
+        is_maria_turns = True
 
-        while primesSet:
+        while primes_set:
             # Maria and Ben alternate turns; they remove multiples of primes
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+            smallest_prime = primes_set.pop(0)
+            rounds_set.remove(smallest_prime)
+            rounds_set = [x for x in rounds_set if x % smallest_prime != 0]
 
-            isMariaTurns = not isMariaTurns
+            is_maria_turns = not is_maria_turns
 
         # Determine who made the last valid move
-        if isMariaTurns:
-            benWinsCount += 1
+        if is_maria_turns:
+            ben_wins_count += 1
         else:
-            mariaWinsCount += 1
+            maria_wins_count += 1
 
     # Return the overall winner
-    if mariaWinsCount > benWinsCount:
+    if maria_wins_count > ben_wins_count:
         return "Maria"
-    elif benWinsCount > mariaWinsCount:
+    elif ben_wins_count > maria_wins_count:
         return "Ben"
     else:
         return None
@@ -56,5 +57,4 @@ def is_prime(n):
 
 def primes_in_range(start, end):
     """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end + 1) if is_prime(n)]
-    return primes
+    return [n for n in range(start, end + 1) if is_prime(n)]
